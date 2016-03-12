bacbda038b4008849eadf708fb0cd55aed1307ab
iSs <tharnchai@hotmail.com>
2016-03-12 14:42:21 -0800

    ทำการเพิ่มซาวด์ปุ่ม

--- app/src/main/java/apptcvr/iss/mytraffic/MainActivity.java
+++ app/src/main/java/apptcvr/iss/mytraffic/MainActivity.java
@@ -1,13 +1,50 @@
 package apptcvr.iss.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        // button controller
+        buttonController();
+
+    }   // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.mosquito);
+                mediaPlayer.start();
+
+            }
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+
+}   // main class นี่คือ คลาสหลัก
