8d43ae2adec802117f765496f70d9b670b5996d8
iSs <tharnchai@hotmail.com>
2016-03-13 15:47:48 -0700

    Show View Successfull

--- app/src/main/java/apptcvr/iss/mytraffic/DetailActivity.java
+++ app/src/main/java/apptcvr/iss/mytraffic/DetailActivity.java
@@ -2,12 +2,49 @@ package apptcvr.iss.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTexview, detailTextView;
+    private ImageView trafficImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Bind Widget
+        bindWidget();
+
+
+        //Show View
+        showView();
+
+    }   //Main Method
+
+    private void showView() {
+
+        String strTitle = getIntent().getStringExtra("Title");
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        titleTexview.setText(strTitle);
+        trafficImageView.setImageResource(intIcon);
+
+        String[] detailStrings = getResources().getStringArray(R.array.detail_long);
+        detailTextView.setText(detailStrings[intIndex]);
+
+    }   // showView
+
+    private void bindWidget() {
+        titleTexview = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+
     }
-}
+
+
+}   // Main Class
--- app/src/main/java/apptcvr/iss/mytraffic/MainActivity.java
+++ app/src/main/java/apptcvr/iss/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void creatListView() {
 
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
                 R.drawable.traffic_04, R.drawable.traffic_05, R.drawable.traffic_06,
                 R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09,
                 R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_16, R.drawable.traffic_17, R.drawable.traffic_18,
                 R.drawable.traffic_19, R.drawable.traffic_20};
 
-        String[] titleString = new String[20];
+        final String[] titleString = new String[20];
         titleString[0] = "หัวข้อหลัก 1";
         titleString[1] = "หัวข้อหลัก 2";
         titleString[2] = "หัวข้อหลัก 3";
@@ -64,13 +65,25 @@ public class MainActivity extends AppCompatActivity {
         titleString[18] = "หัวข้อหลัก 19";
         titleString[19] = "หัวข้อหลัก 20";
 
-        String[] detailString = getResources().getStringArray(R.array.detail_short);
+        final String[] detailString = getResources().getStringArray(R.array.detail_short);
 
 
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, intIcon, titleString, detailString);
         trafficListView.setAdapter(myAdapter);
 
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
 
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("Title", titleString[i]);
+                intent.putExtra("Image", intIcon[i]);
+                intent.putExtra("Index", i);
+                startActivity(intent);
+
+
+            }
+        });
 
     }   //CreateListView
 
